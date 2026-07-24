9f0a2291e7741b642c88dba61df400b8012497be
ogulcanes <gunesogulcan1@gmail.com>
2026-07-24 12:22:16 +0300

    Üretilen özgün logoyu siteye birebir uygula

--- server.js
+++ server.js
@@ -1164,10 +1164,7 @@ async function renderHeader(active) {
     <header class="site-header">
       <div class="container header-main">
         <a class="logo printable-logo" href="/" aria-label="Printable ana sayfa">
-          <span class="printable-logo__mark" aria-hidden="true">
-            <i></i><i></i><i></i>
-          </span>
-          <span class="printable-logo__word">Printable</span>
+          <img src="/assets/printable-logo.png" alt="Printable">
         </a>
         <!-- Yalnızca mobilde görünür; menüyü açar. -->
         <button class="nav-toggle" type="button" id="nav-toggle"
